d2ca2422c7238f167aa7f5c9449465b26068fb8a
BartikaKumar <79638913+BartikaKumar@users.noreply.github.com>
2021-02-26 15:11:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
-function add(x,y){
-  return x+y;
+async function alert(options){
+  // --Alert Box--
+  let alertBox = document.createElement("div");
+  if(options.darkMode){
+    alertBox.className='alertBoxDark';
+  }
+  else{
+    alertBox.className='alertBoxLight';
+  }
+  let children= [];
+  // --Alert Title--
+  let alertTitle = document.createElement('div');
+  alertTitle.className = 'alertTitle';
+  alertTitle.innerText = options.title;
+  children.push(alertTitle);
+  //--Alert Content--
+  let alertContent = document.createElement('div');
+  alertContent.className='alertContent';
+  if(options.hasOwnProperty('html')){
+    alertContent.innerHtml= options.html;
+  }
+  else if(options.hasOwnProperty('text')){
+    alertContent.innerText= options.text;
+  }
+  chidren.push(alertContent);
+  // --Confirm Button--
+  let confirmButton = document.createElement("div");
+  confirmButton.className = "confirmButton";
+  if(options.hasOwnProperty('confirmButtonText')){
+    confirmButton.innerText= options.confirmButtonText;
+  }
+  else{
+    confirmButton.innerText= 'OK';
+  }
+  children.push(confirmButton);
+  // --Secondary Button--
+  if(options.secondaryButton){
+    let secondaryButton = document.createElement('div');
+    var defaultSecondaryButtonText;
+    if(options.secondaryButtonMode=="deny"){
+      secondaryButton.className='denyButton';
+      defaultSecondaryButtonText='No';
+    }
+    else{
+      secondaryButton.className='cancelButton';
+      defaultSecondaryButtonText='Cancel';
+    }
+    if(options.hasOwnProperty('secondaryButtonText')){
+      secondaryButton.innerText= options.secondaryButtonText;
+    }
+    else{
+      secondaryButton.innerText= defaultSecondaryButtonText;
+    }
+    children.push(secondaryButton);
+  }
+  // --Afterwards--
 }
